f3d9126e615cafeeceac0c43eaa8ece085e4d50d
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-01-27 13:55:54 -0300

    iniciando parte de probabilidade das palavras 😣

--- training.py
+++ training.py
@@ -1,6 +1,6 @@
-from treatment import y_train, x_train
+from treatment import y_train, x_train, organized_words
 
-def probabillity():
+def langs_probabillity():
     y = y_train()
     x = x_train()
     qtd_langs = len(x)
@@ -16,4 +16,29 @@ def probabillity():
 
     return probability_list
 
-print(probabillity())
+def words_probabillity():
+    org_words = organized_words()
+    x = x_train()
+    y = y_train()
+    qtd_words_by_lang = {}
+    words_probabillity_list = {}
+
+    for lang, list in org_words.items():
+        qtd_words_by_lang[lang] = qtd_words_by_lang.get(lang, 0) + len(list)   
+
+    for lang, words in org_words.items():
+        idx = 0
+        counter = 0
+        lang_probabillity = {}
+
+        for word in words:
+            for w in words:
+                if w == word:
+                    counter+=1
+            lang_probabillity[word] = round(counter / len(words), 2)
+
+        words_probabillity_list[lang] = lang_probabillity
+
+    return words_probabillity_list
+
+print(words_probabillity())
