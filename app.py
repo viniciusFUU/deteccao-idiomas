6ca023edaf0f86d5db8ef96405f313634726d445
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-02-10 13:36:04 -0300

    funcionalidade adicionar palavra

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from treatment import y_train
+from treatment import y_train, organized_words
 import os
 import mysql.connector
 
@@ -20,3 +20,28 @@ def add_lang(sigla, lang):
         conn.commit()
         
         return "Idioma inserido!"
+
+def add_word(text):
+    words = organized_words()
+    langs = y_train()
+
+    sigla = input("sigla: ")
+
+    for lang, word in words.items():
+        if sigla not in langs:
+            input_lang = input("linguagem: ")
+
+            add_lang(sigla, input_lang)
+
+        if sigla in langs:
+            if sigla == lang:
+                if text not in word:
+                    cursor = conn.cursor()
+                    insert_word = f"INSERT INTO texto (texto, cod_idioma) VALUES ('{text}', '{sigla}')"
+
+                    cursor.execute(insert_word)
+                    conn.commit()
+
+                    return f"{text} adicionado com sucesso"
+                else:
+                    return f"{text} já existe na lista"
