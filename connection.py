6ee5ef114e9e917dd8672bb86260ca2657998801
Vinícius Monteiro <vinicius3_1998@hotmail.com>
2025-02-10 13:36:29 -0300

    requisição de idiomas

--- connection.py
+++ connection.py
@@ -22,3 +22,21 @@ def get_all_words():
     conn.close()
 
     return dados
+
+def get_all_langs():
+    conn = mysql.connector.connect(
+        host=os.getenv("HOST"),
+        port=os.getenv("PORT"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE")
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT cod_idioma FROM idiomas")
+
+    dados = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return dados
